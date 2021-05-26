4de99e0205834505eff199f639647e2c8693a8fd
evelynespinozamacias <63021422+evelynespinozamacias@users.noreply.github.com>
2021-05-25 20:47:42 -0500

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,14 @@
+public class Assignment {
+    public static void main(String[] args)  {
+        String firstName = "Evelyn"; 
+
+        String lastName = " Espinoza-Macias"; 
+
+        String city = "Omaha";
+
+        String fourthString = firstName + lastName; 
+
+        System.out.println(fourthString);
+        System.out.println(city);
+    }
+}
